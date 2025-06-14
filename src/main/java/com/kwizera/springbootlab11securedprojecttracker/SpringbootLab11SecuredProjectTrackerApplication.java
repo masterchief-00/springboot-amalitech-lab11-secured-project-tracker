f09250b7bbc0078e4129a55e7be7fb2c9ef1502e
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-06-14 12:45:17 +0200

    init commit

---
+++ src/main/java/com/kwizera/springbootlab11securedprojecttracker/SpringbootLab11SecuredProjectTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.kwizera.springbootlab11securedprojecttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLab11SecuredProjectTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLab11SecuredProjectTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwizera/springbootlab11securedprojecttracker/SpringbootLab11SecuredProjectTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwizera.springbootlab11securedprojecttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLab11SecuredProjectTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
